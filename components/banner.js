11648404290c47a57d9003ca0c15e98310cb4d9e
SohaibArbiBakcha <sohaibbakcha@gmail.com>
2022-07-06 17:52:33 +0100

    add banner components

---
+++ components/banner.js
@@ -0,0 +1,23 @@
+﻿import React from "react";
+import styles from "./banner.module.css";
+const Banner = ({ buttonText, handleOnClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>{" "}
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subtitle}>Descover your local coffes shops!</p>
+      <button
+        className={styles.button}
+        onClick={() => {
+          console.log("You click me UwU");
+        }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/hello.js
+++
@@ -1,7 +0,0 @@
-﻿import React from "react";
-
-const hello = () => {
-  return <div>Hello World</div>;
-};
-
-export default hello;
