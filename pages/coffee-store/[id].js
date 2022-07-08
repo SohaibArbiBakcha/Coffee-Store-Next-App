5cfff8c8042fb22001151327c3d8526cc481a439
SohaibArbiBakcha <sohaibbakcha@gmail.com>
2022-07-08 17:48:20 +0100

    add styling and learn about dynamic pages

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,10 @@
+﻿import { useRouter } from "next/router";
+import React from "react";
+
+const CoffeeStore = () => {
+  const { query } = useRouter();
+  // console.log(router);
+  return <div>CoffeeStore Page : {query.id}</div>;
+};
+
+export default CoffeeStore;
