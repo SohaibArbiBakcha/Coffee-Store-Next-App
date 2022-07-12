5a5c39fc0534c201086b36b291d71ec9ad781fa7
SohaibArbiBakcha <sohaibbakcha@gmail.com>
2022-07-12 17:58:52 +0100

    workin on the banner

--- components/banner.js
+++ components/banner.js
@@ -7,15 +7,12 @@ const Banner = ({ buttonText, handleOnClick }) => {
         <span className={styles.title1}>Coffee</span>{" "}
         <span className={styles.title2}>Connoisseur</span>
       </h1>
-      <p className={styles.subtitle}>Descover your local coffes shops!</p>
-      <button
-        className={styles.button}
-        onClick={() => {
-          console.log("You click me UwU");
-        }}
-      >
-        {buttonText}
-      </button>
+      <p className={styles.subTitle}>Descover your local coffes shops!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleOnClick}>
+          {buttonText}
+        </button>
+      </div>
     </div>
   );
 };
--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,15 @@
 ﻿import { useRouter } from "next/router";
+import Link from "next/link";
 import React from "react";
 
 const CoffeeStore = () => {
   const { query } = useRouter();
   // console.log(router);
-  return <div>CoffeeStore Page : {query.id}</div>;
+  return (
+    <div>
+      CoffeeStore Page : {query.id} <br /> <Link href="/">Back to home</Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
